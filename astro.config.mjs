d63124ae2c88ade87ca28506d56c941285b65980
Alejo30 <43772749+Alejo30@users.noreply.github.com>
2023-06-09 12:54:18 -0500

    modificando

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Alejo30.github.io',
+  site: 'https://alejo30.github.io',
   base: '/astro-landing-curso',
 });
